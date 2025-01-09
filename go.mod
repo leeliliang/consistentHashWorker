04cc9506b9cad8956fe66ddcefc20a5254a70891
jiawood <jiawood@fiveelements.com>
2025-01-09 19:02:57 +0800

    fix: add pool

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/leeliliang/consistenHashWorker
 
 go 1.23.1
 
-require github.com/spaolacci/murmur3 v1.1.0
+require (
+	github.com/spaolacci/murmur3 v1.1.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ worker/taskPool.go
@@ -0,0 +1,68 @@
+package worker
+
+import (
+	"go.uber.org/zap"
+	"sync"
+)
+
+type WorkerPool struct {
+	poolSize   int
+	taskName   []string
+	tasks      map[string]chan Task // 每个 hash 值对应一个任务通道
+	wg         sync.WaitGroup
+	consistent *ConsistentHash
+	logger     *zap.Logger
+}
+
+func NewWorkerPool(poolSize int, taskName []string, logger *zap.Logger) *WorkerPool {
+	tasks := make(map[string]chan Task, poolSize)
+	for _, value := range taskName {
+		tasks[value] = make(chan Task, 1000)
+	}
+
+	consistentIns := New(poolSize, MurmurHash)
+	consistentIns.Add(taskName)
+	logger.Info("taskName", zap.Any("taskName", taskName))
+	return &WorkerPool{
+		poolSize:   poolSize,
+		tasks:      tasks,
+		taskName:   taskName,
+		consistent: consistentIns,
+		logger:     logger,
+	}
+}
+
+func (wp *WorkerPool) Start() {
+	for index, value := range wp.taskName {
+		wp.wg.Add(1)
+		wp.logger.Info("start", zap.Int("index", index), zap.String("value", value))
+		go wp.worker(value)
+	}
+}
+
+func (wp *WorkerPool) worker(workerID string) {
+	wp.logger.Debug("task", zap.String("workerID", workerID))
+	defer wp.wg.Done()
+	for task := range wp.tasks[workerID] {
+		//wp.logger.Debug("task", zap.String("workerID", workerID), zap.String("task", string(task.Data)))
+		err := task.HandlerMessage()
+		if err != nil {
+			wp.logger.Error("Failed to process record message", zap.Error(err))
+			continue
+		}
+
+	}
+}
+
+func (wp *WorkerPool) AddTask(task Task, hashKey string) {
+	key := wp.consistent.Get(hashKey)
+	//wp.logger.Info("msg", zap.String("key", key), zap.String("hashKey", hashKey))
+	wp.tasks[key] <- task
+}
+
+func (wp *WorkerPool) Wait() {
+	for _, ch := range wp.tasks {
+		close(ch)
+	}
+	wp.wg.Wait()
+}
